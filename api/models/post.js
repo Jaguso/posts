d255c0195056f58e4019db26fa2fa51eaf6c1a74
Jaguso <jagutzo@gmail.com>
2019-06-13 20:10:30 -0500

    made post model

---
+++ api/models/post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: String,
+    postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [{
+        text: String,
+        postedBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }]
+});
+
+module.exports = mongoose.model("Post", PostSchema);
