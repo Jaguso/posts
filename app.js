a5bb315bc5eea0f12631e8c45b4a76b9828a6f22
Jaguso <jagutzo@gmail.com>
2019-06-14 19:43:10 -0500

    b

--- app.js
+++ app.js
@@ -11,50 +11,46 @@ mongoose.connect(
 );
 
 
-app.get('/', (req, res) => {
-    res.send("it works");
-});
+// app.get('/', (req, res) => {
+//     res.send("it works");
+// });
 
-var alex = new User({
-    name: "Alex"
-});
+// var alex = new User({
+//     name: "Alex"
+// });
 
-var joe = new User({
-    name: "Joe"
-});
-
-
-alex.save();
-joe.save();
+// var joe = new User({
+//     name: "Joe"
+// });
 
 
-var post = new Post({
-    title: "Hello world",
-    postedBy: alex._id,
-    comments: [{
-        text: "Nice ass",
-        postedBy: joe._id
-    }, 
-    {
-        text: "Thanks",
-        postedBy: alex._id
-    }]
-});
-
-post.save(function(error) {
-    if (!error) {
-        Post.find({})
-            .populate('postedBy')
-            .populate('comments.postedBy')
-            .exec(function(error, post) {
-                console.log(JSON.stringify(post, null, "\t" ))
-            })
-    }
-})
+// alex.save();
+// joe.save();
 
 
+// var post = new Post({
+//     title: "Hello world",
+//     postedBy: alex._id,
+//     comments: [{
+//         text: "Nice ass",
+//         postedBy: joe._id
+//     }, 
+//     {
+//         text: "Thanks",
+//         postedBy: alex._id
+//     }]
+// });
 
-
+// post.save(function(error) {
+//     if (!error) {
+//         Post.find({})
+//             .populate('postedBy')
+//             .populate('comments.postedBy')
+//             .exec(function(error, post) {
+//                 console.log(JSON.stringify(post, null, "\t" ))
+//             })
+//     }
+// })
 
 
 module.exports = app;
